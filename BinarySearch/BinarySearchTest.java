4e108e76d9c781ddde0ed8a7d5183f9c2a0f1471
Nate <nateliu76@gmail.com>
2015-09-07 16:31:29 -0700

    Update BinarySearchTest.java

--- BinarySearch/BinarySearchTest.java
+++ BinarySearch/BinarySearchTest.java
@@ -1,149 +1,287 @@
 import junit.framework.TestCase;
+import java.util.Comparator;
 
-/**
- * A JUnit test case class.
- * Every method starting with the word "test" will be called when running
- * the test with JUnit.
- */
 public class BinarySearchTest extends TestCase {
-    // small arrays to test edge cases and boundary conditions
+    
     int[] a1 = {10, 11};
     int[] a2 = {10};
-    
-    // a more common sorted array to test functionality
-    int[] a5 = {10, 10, 11, 13, 18, 23, 23, 48, 50, 54, 68, 77, 77, 77, 84, 98,
+    int[] a3 = {10, 10, 11, 13, 18, 23, 23, 48, 50, 54, 68, 77, 77, 77, 84, 98,
         98, 99};
     
+    Integer[] A1 = convert(a1);
+    Integer[] A2 = convert(a2);
+    Integer[] A3 = convert(a3);
+    Comparator<Integer> comp = new IntOrder();
+    
+    private class IntOrder implements Comparator<Integer> {
+        public int compare(Integer a, Integer b) {
+            return Integer.compare(a, b);
+        }
+    }
+    
+    private Integer[] convert(int[] a) {
+        Integer[] A = new Integer[a.length];
+        int idx = 0;
+        for (int i : a) A[idx++] = Integer.valueOf(i);
+        return A;
+    }
+    
     public void testIndexOf() {
-        assertEquals(BinarySearch.indexOf(10, a1), 0);
-        assertEquals(BinarySearch.indexOf(12, a1), -1);
-        assertEquals(BinarySearch.indexOf(4, a1), -1);
+        assertEquals(BinarySearch.indexOf(A1, 10, comp), 0);
+        assertEquals(BinarySearch.indexOf(A1, 11, comp), 1);
+        assertEquals(BinarySearch.indexOf(A1, 12, comp), -1);
+        assertEquals(BinarySearch.indexOf(A1, 4, comp), -1);
         
-        assertEquals(BinarySearch.indexOf(10, a2), 0);
-        assertEquals(BinarySearch.indexOf(12, a2), -1);
-        assertEquals(BinarySearch.indexOf(4, a2), -1);
+        assertEquals(BinarySearch.indexOf(A2, 10, comp), 0);
+        assertEquals(BinarySearch.indexOf(A2, 12, comp), -1);
+        assertEquals(BinarySearch.indexOf(A2, 4, comp), -1);
         
-        int n10 = BinarySearch.indexOf(10, a5);
+        int n10 = BinarySearch.indexOf(A3, 10, comp);
         assertTrue(n10 >= 0 && n10 <= 1);
-        assertEquals(BinarySearch.indexOf(12, a5), -1);
-        assertEquals(BinarySearch.indexOf(4, a5), -1);
+        assertEquals(BinarySearch.indexOf(A3, 12, comp), -1);
+        assertEquals(BinarySearch.indexOf(A3, 4, comp), -1);
         
-        int n77 = BinarySearch.indexOf(77, a5);
+        int n77 = BinarySearch.indexOf(A3, 77, comp);
         assertTrue(n77 >= 11 && n77 <= 13);
+        
+        assertEquals(BinarySearch.indexOf(A1, 10), 0);
+        assertEquals(BinarySearch.indexOf(A1, 11), 1);
+        assertEquals(BinarySearch.indexOf(A1, 12), -1);
+        assertEquals(BinarySearch.indexOf(A1, 4), -1);
+        
+        assertEquals(BinarySearch.indexOf(A2, 10), 0);
+        assertEquals(BinarySearch.indexOf(A2, 12), -1);
+        assertEquals(BinarySearch.indexOf(A2, 4), -1);
+        
+        int n10_1 = BinarySearch.indexOf(A3, 10);
+        assertTrue(n10_1 >= 0 && n10_1 <= 1);
+        assertEquals(BinarySearch.indexOf(A3, 12), -1);
+        assertEquals(BinarySearch.indexOf(A3, 4), -1);
+        
+        int n77_1 = BinarySearch.indexOf(A3, 77);
+        assertTrue(n77_1 >= 11 && n77_1 <= 13);
         
     }
     
     public void testFirstIndexOf() {
-        assertEquals(BinarySearch.firstIndexOf(10, a1), 0);
-        assertEquals(BinarySearch.firstIndexOf(11, a1), 1);
-        assertEquals(BinarySearch.firstIndexOf(12, a1), -1);
-        assertEquals(BinarySearch.firstIndexOf(4, a1), -1);
+        assertEquals(BinarySearch.firstIndexOf(A1, 10, comp), 0);
+        assertEquals(BinarySearch.firstIndexOf(A1, 11, comp), 1);
+        assertEquals(BinarySearch.firstIndexOf(A1, 12, comp), -3);
+        assertEquals(BinarySearch.firstIndexOf(A1, 4, comp), -1);
         
-        assertEquals(BinarySearch.firstIndexOf(10, a5), 0);
-        assertEquals(BinarySearch.firstIndexOf(77, a5), 11);
-        assertEquals(BinarySearch.firstIndexOf(99, a5), 17);
+        assertEquals(BinarySearch.firstIndexOf(A2, 10, comp), 0);
+        assertEquals(BinarySearch.firstIndexOf(A2, 12, comp), -2);
+        assertEquals(BinarySearch.firstIndexOf(A2, 4, comp), -1);
+        
+        assertEquals(BinarySearch.firstIndexOf(A3, 10, comp), 0);
+        assertEquals(BinarySearch.firstIndexOf(A3, 77, comp), 11);
+        assertEquals(BinarySearch.firstIndexOf(A3, 99, comp), 17);
+        
+        assertEquals(BinarySearch.firstIndexOf(A1, 10), 0);
+        assertEquals(BinarySearch.firstIndexOf(A1, 11), 1);
+        assertEquals(BinarySearch.firstIndexOf(A1, 12), -3);
+        assertEquals(BinarySearch.firstIndexOf(A1, 4), -1);
+        
+        assertEquals(BinarySearch.firstIndexOf(A2, 10), 0);
+        assertEquals(BinarySearch.firstIndexOf(A2, 12), -2);
+        assertEquals(BinarySearch.firstIndexOf(A2, 4), -1);
+        
+        assertEquals(BinarySearch.firstIndexOf(A3, 10), 0);
+        assertEquals(BinarySearch.firstIndexOf(A3, 77), 11);
+        assertEquals(BinarySearch.firstIndexOf(A3, 99), 17);
     }
     
     public void testLastIndexOf() {
-        // normal test cases
-        assertEquals(BinarySearch.lastIndexOf(10, a1), 0);
-        assertEquals(BinarySearch.lastIndexOf(11, a1), 1);
-        assertEquals(BinarySearch.lastIndexOf(12, a1), -1);
-        assertEquals(BinarySearch.lastIndexOf(4, a1), -1);
+        assertEquals(BinarySearch.lastIndexOf(A1, 10, comp), 0);
+        assertEquals(BinarySearch.lastIndexOf(A1, 11, comp), 1);
+        assertEquals(BinarySearch.lastIndexOf(A1, 12, comp), -3);
+        assertEquals(BinarySearch.lastIndexOf(A1, 4, comp), -1);
         
-        // in larger arrays
-        assertEquals(BinarySearch.lastIndexOf(10, a5), 1);
-        assertEquals(BinarySearch.lastIndexOf(77, a5), 13);
-        assertEquals(BinarySearch.lastIndexOf(99, a5), 17);
+        assertEquals(BinarySearch.lastIndexOf(A2, 10, comp), 0);
+        assertEquals(BinarySearch.lastIndexOf(A2, 12, comp), -2);
+        assertEquals(BinarySearch.lastIndexOf(A2, 4, comp), -1);
+        
+        assertEquals(BinarySearch.lastIndexOf(A3, 10, comp), 1);
+        assertEquals(BinarySearch.lastIndexOf(A3, 77, comp), 13);
+        assertEquals(BinarySearch.lastIndexOf(A3, 99, comp), 17);
+        
+        assertEquals(BinarySearch.lastIndexOf(A1, 10), 0);
+        assertEquals(BinarySearch.lastIndexOf(A1, 11), 1);
+        assertEquals(BinarySearch.lastIndexOf(A1, 12), -3);
+        assertEquals(BinarySearch.lastIndexOf(A1, 4), -1);
+        
+        assertEquals(BinarySearch.lastIndexOf(A2, 10), 0);
+        assertEquals(BinarySearch.lastIndexOf(A2, 12), -2);
+        assertEquals(BinarySearch.lastIndexOf(A2, 4), -1);
+        
+        assertEquals(BinarySearch.lastIndexOf(A3, 10), 1);
+        assertEquals(BinarySearch.lastIndexOf(A3, 77), 13);
+        assertEquals(BinarySearch.lastIndexOf(A3, 99), 17);
     }
     
     public void testFloor() {
-        assertEquals(BinarySearch.floor(10, a1), 0);
-        assertEquals(BinarySearch.floor(11, a1), 1);
-        assertEquals(BinarySearch.floor(12, a1), 1);
-        assertEquals(BinarySearch.floor(9, a1), -1);
+        assertEquals(BinarySearch.floor(A1, 10, comp), 0);
+        assertEquals(BinarySearch.floor(A1, 11, comp), 1);
+        assertEquals(BinarySearch.floor(A1, 12, comp), 1);
+        assertEquals(BinarySearch.floor(A1, 9, comp), -1);
         
-        assertEquals(BinarySearch.floor(10, a2), 0);
-        assertEquals(BinarySearch.floor(9, a2), -1);
-        assertEquals(BinarySearch.floor(12, a2), 0);
+        assertEquals(BinarySearch.floor(A2, 10, comp), 0);
+        assertEquals(BinarySearch.floor(A2, 9, comp), -1);
+        assertEquals(BinarySearch.floor(A2, 12, comp), 0);
         
-        assertEquals(BinarySearch.floor(80, a5), 13);
+        assertEquals(BinarySearch.floor(A3, 80, comp), 13);
+        
+        assertEquals(BinarySearch.floor(A1, 10), 0);
+        assertEquals(BinarySearch.floor(A1, 11), 1);
+        assertEquals(BinarySearch.floor(A1, 12), 1);
+        assertEquals(BinarySearch.floor(A1, 9), -1);
+        
+        assertEquals(BinarySearch.floor(A2, 10), 0);
+        assertEquals(BinarySearch.floor(A2, 9), -1);
+        assertEquals(BinarySearch.floor(A2, 12), 0);
+        
+        assertEquals(BinarySearch.floor(A3, 80), 13);
     }
-    
+
     public void testCeil() {
-        assertEquals(BinarySearch.ceil(10, a1), 0);
-        assertEquals(BinarySearch.ceil(11, a1), 1);
-        assertEquals(BinarySearch.ceil(12, a1), -1);
-        assertEquals(BinarySearch.ceil(9, a1), 0);
+        assertEquals(BinarySearch.ceiling(A1, 10, comp), 0);
+        assertEquals(BinarySearch.ceiling(A1, 11, comp), 1);
+        assertEquals(BinarySearch.ceiling(A1, 12, comp), -1);
+        assertEquals(BinarySearch.ceiling(A1, 9, comp), 0);
         
-        assertEquals(BinarySearch.ceil(10, a2), 0);
-        assertEquals(BinarySearch.ceil(9, a2), 0);
-        assertEquals(BinarySearch.ceil(12, a2), -1);
+        assertEquals(BinarySearch.ceiling(A2, 10, comp), 0);
+        assertEquals(BinarySearch.ceiling(A2, 9, comp), 0);
+        assertEquals(BinarySearch.ceiling(A2, 12, comp), -1);
         
-        assertEquals(BinarySearch.ceil(20, a5), 5);
+        assertEquals(BinarySearch.ceiling(A3, 20, comp), 5);
+        
+        assertEquals(BinarySearch.ceiling(A1, 10), 0);
+        assertEquals(BinarySearch.ceiling(A1, 11), 1);
+        assertEquals(BinarySearch.ceiling(A1, 12), -1);
+        assertEquals(BinarySearch.ceiling(A1, 9), 0);
+        
+        assertEquals(BinarySearch.ceiling(A2, 10), 0);
+        assertEquals(BinarySearch.ceiling(A2, 9), 0);
+        assertEquals(BinarySearch.ceiling(A2, 12), -1);
+        
+        assertEquals(BinarySearch.ceiling(A3, 20), 5);
     }
     
     public void testPred() {
-        assertEquals(BinarySearch.pred(10, a1), -1);
-        assertEquals(BinarySearch.pred(11, a1), 0);
-        assertEquals(BinarySearch.pred(12, a1), 1);
-        assertEquals(BinarySearch.pred(9, a1), -1);
+        assertEquals(BinarySearch.predecessor(A1, 10, comp), -1);
+        assertEquals(BinarySearch.predecessor(A1, 11, comp), 0);
+        assertEquals(BinarySearch.predecessor(A1, 12, comp), 1);
+        assertEquals(BinarySearch.predecessor(A1, 9, comp), -1);
         
-        assertEquals(BinarySearch.pred(10, a5), -1);
-        assertEquals(BinarySearch.pred(40, a5), 6);
-        assertEquals(BinarySearch.pred(100, a5), 17);
-        assertEquals(BinarySearch.pred(99, a5), 16);
+        assertEquals(BinarySearch.predecessor(A3, 10, comp), -1);
+        assertEquals(BinarySearch.predecessor(A3, 40, comp), 6);
+        assertEquals(BinarySearch.predecessor(A3, 100, comp), 17);
+        assertEquals(BinarySearch.predecessor(A3, 99, comp), 16);
+        
+        assertEquals(BinarySearch.predecessor(A1, 10), -1);
+        assertEquals(BinarySearch.predecessor(A1, 11), 0);
+        assertEquals(BinarySearch.predecessor(A1, 12), 1);
+        assertEquals(BinarySearch.predecessor(A1, 9), -1);
+        
+        assertEquals(BinarySearch.predecessor(A3, 10), -1);
+        assertEquals(BinarySearch.predecessor(A3, 40), 6);
+        assertEquals(BinarySearch.predecessor(A3, 100), 17);
+        assertEquals(BinarySearch.predecessor(A3, 99), 16);
     }
     
     public void testSucc() {
-        assertEquals(BinarySearch.succ(10, a1), 1);
-        assertEquals(BinarySearch.succ(11, a1), -1);
-        assertEquals(BinarySearch.succ(12, a1), -1);
-        assertEquals(BinarySearch.succ(9, a1), 0);
+        assertEquals(BinarySearch.successor(A1, 10, comp), 1);
+        assertEquals(BinarySearch.successor(A1, 11, comp), -1);
+        assertEquals(BinarySearch.successor(A1, 12, comp), -1);
+        assertEquals(BinarySearch.successor(A1, 9, comp), 0);
         
-        assertEquals(BinarySearch.succ(10, a5), 2);
-        assertEquals(BinarySearch.succ(68, a5), 11);
-        assertEquals(BinarySearch.succ(100, a5), -1);
-        assertEquals(BinarySearch.succ(99, a5), -1);
+        assertEquals(BinarySearch.successor(A3, 10, comp), 2);
+        assertEquals(BinarySearch.successor(A3, 68, comp), 11);
+        assertEquals(BinarySearch.successor(A3, 100, comp), -1);
+        assertEquals(BinarySearch.successor(A3, 99, comp), -1);
+        
+        assertEquals(BinarySearch.successor(A1, 10), 1);
+        assertEquals(BinarySearch.successor(A1, 11), -1);
+        assertEquals(BinarySearch.successor(A1, 12), -1);
+        assertEquals(BinarySearch.successor(A1, 9), 0);
+        
+        assertEquals(BinarySearch.successor(A3, 10), 2);
+        assertEquals(BinarySearch.successor(A3, 68), 11);
+        assertEquals(BinarySearch.successor(A3, 100), -1);
+        assertEquals(BinarySearch.successor(A3, 99), -1);
     }
     
     public void testCount() {
-        assertEquals(BinarySearch.count(10, a1), 1);
-        assertEquals(BinarySearch.count(11, a1), 1);
-        assertEquals(BinarySearch.count(9, a1), 0);
-        assertEquals(BinarySearch.count(12, a1), 0);
+        assertEquals(BinarySearch.count(A1, 10, comp), 1);
+        assertEquals(BinarySearch.count(A1, 11, comp), 1);
+        assertEquals(BinarySearch.count(A1, 9, comp), 0);
+        assertEquals(BinarySearch.count(A1, 12, comp), 0);
         
-        assertEquals(BinarySearch.count(77, a5), 3);
-        assertEquals(BinarySearch.count(99, a5), 1);
-        assertEquals(BinarySearch.count(10, a5), 2);
-        assertEquals(BinarySearch.count(33, a5), 0);
+        assertEquals(BinarySearch.count(A3, 77, comp), 3);
+        assertEquals(BinarySearch.count(A3, 99, comp), 1);
+        assertEquals(BinarySearch.count(A3, 10, comp), 2);
+        assertEquals(BinarySearch.count(A3, 33, comp), 0);
+        
+        assertEquals(BinarySearch.count(A1, 10), 1);
+        assertEquals(BinarySearch.count(A1, 11), 1);
+        assertEquals(BinarySearch.count(A1, 9), 0);
+        assertEquals(BinarySearch.count(A1, 12), 0);
+        
+        assertEquals(BinarySearch.count(A3, 77), 3);
+        assertEquals(BinarySearch.count(A3, 99), 1);
+        assertEquals(BinarySearch.count(A3, 10), 2);
+        assertEquals(BinarySearch.count(A3, 33), 0);
     }
     
     public void testRank() {
-        assertEquals(BinarySearch.rank(10, a1), 0);
-        assertEquals(BinarySearch.rank(11, a1), 1);
-        assertEquals(BinarySearch.rank(12, a1), 2);
-        assertEquals(BinarySearch.rank(9, a1), 0);
+        assertEquals(BinarySearch.rank(A1, 10, comp), 0);
+        assertEquals(BinarySearch.rank(A1, 11, comp), 1);
+        assertEquals(BinarySearch.rank(A1, 12, comp), 2);
+        assertEquals(BinarySearch.rank(A1, 9, comp), 0);
         
-        assertEquals(BinarySearch.rank(10, a2), 0);
-        assertEquals(BinarySearch.rank(11, a2), 1);
-        assertEquals(BinarySearch.rank(9, a2), 0);
+        assertEquals(BinarySearch.rank(A2, 10, comp), 0);
+        assertEquals(BinarySearch.rank(A2, 11, comp), 1);
+        assertEquals(BinarySearch.rank(A2, 9, comp), 0);
         
-        assertEquals(BinarySearch.rank(77, a5), 11);
-        assertEquals(BinarySearch.rank(10, a5), 0);
-        assertEquals(BinarySearch.rank(100, a5), 18);
-        assertEquals(BinarySearch.rank(51, a5), 9);
+        assertEquals(BinarySearch.rank(A3, 77, comp), 11);
+        assertEquals(BinarySearch.rank(A3, 10, comp), 0);
+        assertEquals(BinarySearch.rank(A3, 100, comp), 18);
+        assertEquals(BinarySearch.rank(A3, 51, comp), 9);
+        
+        assertEquals(BinarySearch.rank(A1, 10), 0);
+        assertEquals(BinarySearch.rank(A1, 11), 1);
+        assertEquals(BinarySearch.rank(A1, 12), 2);
+        assertEquals(BinarySearch.rank(A1, 9), 0);
+        
+        assertEquals(BinarySearch.rank(A2, 10), 0);
+        assertEquals(BinarySearch.rank(A2, 11), 1);
+        assertEquals(BinarySearch.rank(A2, 9), 0);
+        
+        assertEquals(BinarySearch.rank(A3, 77), 11);
+        assertEquals(BinarySearch.rank(A3, 10), 0);
+        assertEquals(BinarySearch.rank(A3, 100), 18);
+        assertEquals(BinarySearch.rank(A3, 51), 9);
     }
     
     public void testRangeCount() {
-        assertEquals(BinarySearch.rangeCount(10, 11, a1), 1);
-        assertEquals(BinarySearch.rangeCount(11, 12, a1), 1);
-        assertEquals(BinarySearch.rangeCount(0, 99, a1), 2);
-        assertEquals(BinarySearch.rangeCount(9, 10, a1), 0);
+        assertEquals(BinarySearch.rangeCount(A1, 10, 11, comp), 1);
+        assertEquals(BinarySearch.rangeCount(A1, 11, 12, comp), 1);
+        assertEquals(BinarySearch.rangeCount(A1, 0, 99, comp), 2);
+        assertEquals(BinarySearch.rangeCount(A1, 9, 10, comp), 0);
         
-        assertEquals(BinarySearch.rangeCount(-10, 1000, a5), 18);
-        assertEquals(BinarySearch.rangeCount(-10, 25, a5), 7);
-        assertEquals(BinarySearch.rangeCount(70, 1000, a5), 7);
-        assertEquals(BinarySearch.rangeCount(25, 70, a5), 4);
+        assertEquals(BinarySearch.rangeCount(A3, -10, 1000, comp), 18);
+        assertEquals(BinarySearch.rangeCount(A3, -10, 25, comp), 7);
+        assertEquals(BinarySearch.rangeCount(A3, 70, 1000, comp), 7);
+        assertEquals(BinarySearch.rangeCount(A3, 25, 70, comp), 4);
+        
+        assertEquals(BinarySearch.rangeCount(A1, 10, 11), 1);
+        assertEquals(BinarySearch.rangeCount(A1, 11, 12), 1);
+        assertEquals(BinarySearch.rangeCount(A1, 0, 99), 2);
+        assertEquals(BinarySearch.rangeCount(A1, 9, 10), 0);
+        
+        assertEquals(BinarySearch.rangeCount(A3, -10, 1000), 18);
+        assertEquals(BinarySearch.rangeCount(A3, -10, 25), 7);
+        assertEquals(BinarySearch.rangeCount(A3, 70, 1000), 7);
+        assertEquals(BinarySearch.rangeCount(A3, 25, 70), 4);
     }
 }
